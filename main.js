92cbb7e577dad2176aa64a79193d3661eb48fbfd
Máté Kotroczó <matedon@gmail.com>
2024-04-29 13:06:59 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import XYZ from 'ol/source/XYZ.js';
+import {fromLonLat} from 'ol/proj.js';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        attributions: [
+          'Powered by Esri',
+          'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community',
+        ],
+        attributionsCollapsible: false,
+        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        maxZoom: 23,
+        zoom: 21,
+      }),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([17.91378, 47.092972]),
+    zoom: 7,
+  }),
+});
